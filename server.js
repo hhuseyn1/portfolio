da8b23065921b361a3749c027f7e20e20f56cea6
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-05-13 20:53:53 +0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require("dotenv").config()
 const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes')
+const projectRoutes = require('./routes/projectRoutes')
 const app = express();
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -9,3 +15,11 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose
+  .connect(DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
+ 
+app.use("/blogs", blogRoutes);
+app.use("/projects", projectRoutes);
