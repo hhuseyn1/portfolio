12b57e66e803b0d11346e8bb4c4f003ae743e7c1
hhuseyn1 <huseyn.hemidov.2004@gmail.com>
2024-05-16 21:00:24 +0400

    complete routers

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,66 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Blog = require("../models/blog");
 
 
-module.exports = router
+router.get("/", async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+        res.json(blogs);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id);
+        res.json(blog)
+    } catch (err) {
+        res.status(404).json({ message: "Blog not found!" });
+    }
+});
+
+router.post("/", async (req, res) => {
+    const blog = new Blog({
+        title: req.body.title,
+        desc: req.body.desc,
+        gallery: req.body.gallery,
+        refLinks: req.body.refLinks,
+        category: req.body.category,
+    });
+    try {
+        const newBlog = await blog.save();
+        res.status(201).json(newBlog);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+router.put("/:id", async (req, res) => {
+    try {
+        const status = await Blog.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            desc: req.body.desc,
+            gallery: req.body.gallery,
+            refLinks: req.body.refLinks,
+            category: req.body.category,
+        });
+        res.json(status ? "Blog updated Successfully!" : "Error occured");
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await Blog.findByIdAndDelete(req.params.id);
+        res.json({ message: "Blog deleted sucessfully" });
+    } catch (err) {
+        req.status(404).json({ message: "Blog not found!" });
+    }
+});
+
+module.exports = router;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,5 +1,71 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Project = require("../models/project");
 
 
-module.exports = router
+router.get("/", async (req, res) => {
+    try {
+        const projects = await Project.find();
+        res.json(projects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const project = await Project.findById(req.params.id);
+        res.json(project);
+    } catch (err) {
+        res.status(404).json({ message: "Project not found!" });
+    }
+});
+
+router.post("/", async (req, res) => {
+    const project = new Project({
+        title: req.body.title,
+        desc: req.body.desc,
+        gallery: req.body.gallery,
+        playStoreLink: req.body.playStoreLink,
+        appStoreLink: req.body.appStoreLink,
+        domain: req.body.domain,
+        githubLink: req.body.githubLink,
+        projectType: req.body.projectType,
+    });
+    try {
+        const newProject = await project.save();
+        res.status(201).json(newProject);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+router.put("/:id", async (req, res) => {
+    try {
+        const status = await Project.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            desc: req.body.desc,
+            gallery: req.body.gallery,
+            playStoreLink: req.body.playStoreLink,
+            appStoreLink: req.body.appStoreLink,
+            domain: req.body.domain,
+            githubLink: req.body.githubLink,
+            projectType: req.body.projectType,
+        });
+        res.json(status ? "Projecy updated Successfully!" : "Error occured");
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await Project.findByIdAndDelete(req.params.id);
+        res.json({ message: "Project deleted sucessfully" });
+    } catch (err) {
+        req.status(404).json({ message: "Project not found!" })
+    }
+});
+
+module.exports = router;
